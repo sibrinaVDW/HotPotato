fa3fea382e018aff487c063dee4a33b9e04ad7e1
ryan-muller <55444161+ryan-muller@users.noreply.github.com>
2021-11-11 16:58:46 +0200

    fixed merge issues

--- app/src/main/java/com/example/hotpotato/MapboxMapActivity.java
+++ app/src/main/java/com/example/hotpotato/MapboxMapActivity.java
@@ -51,7 +51,6 @@ import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
 import java.util.ArrayList;
@@ -80,10 +79,10 @@ import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.style.light.Position;
 import com.mapbox.android.core.location.LocationEngine;
 
-import com.mapbox.search.MapboxSearchSdk;
-import com.mapbox.search.location.DefaultLocationProvider;
-import com.mapbox.maps.MapView;
-import com.mapbox.search.ui.view.SearchBottomSheetView;
+//import com.mapbox.search.MapboxSearchSdk;
+//import com.mapbox.search.location.DefaultLocationProvider;
+//import com.mapbox.maps.MapView;
+//import com.mapbox.search.ui.view.SearchBottomSheetView;
 
 import java.util.List;
 
@@ -141,28 +140,35 @@ public class MapboxMapActivity extends AppCompatActivity implements OnMapReadyCa
                 });
             }
         });
-      
-SearchBottomSheetView searchBottomSheetView = findViewById(R.id.search_view);
-        searchBottomSheetView.initializeSearch(savedInstanceState, new SearchBottomSheetView.Configuration());
+
+//        SearchBottomSheetView searchBottomSheetView = findViewById(R.id.search_view);
+//        searchBottomSheetView.initializeSearch(savedInstanceState, new SearchBottomSheetView.Configuration());
 
     }
-  public void enableLocation() {
+
+    public void enableLocation() {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
             initializeLocationEngine();
             initialLocationLayer();
-        }
-        else{
+        } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
         }
     }
+
+    private void initialLocationLayer() {
+    }
+
+    private void initializeLocationEngine() {
+    }
+
     /**
      * Add the route and marker sources to the map
      */
     private void initSource(@NonNull Style loadedMapStyle) {
         loadedMapStyle.addSource(new GeoJsonSource(ROUTE_SOURCE_ID));
 
-        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[] {
+        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[]{
                 Feature.fromGeometry(Point.fromLngLat(origin.longitude(), origin.latitude())),
                 Feature.fromGeometry(Point.fromLngLat(destination.longitude(), destination.latitude()))}));
         loadedMapStyle.addSource(iconGeoJsonSource);
@@ -192,164 +198,164 @@ SearchBottomSheetView searchBottomSheetView = findViewById(R.id.search_view);
                 iconImage(RED_PIN_ICON_ID),
                 iconIgnorePlacement(true),
                 iconAllowOverlap(true),
-                iconOffset(new Float[] {0f, -9f})));
+                iconOffset(new Float[]{0f, -9f})));
     }
 
-        
+        /**
+         * Make a request to the Mapbox Directions API. Once successful, pass the route to the
+         * route layer.
+         * @param mapboxMap the Mapbox map object that the route will be drawn on
+         * @param origin      the starting point of the route
+         * @param destination the desired finish point of the route
+         */
+        private void getRoute (MapboxMap mapboxMap, Point origin, Point destination){
+            client = MapboxDirections.builder()
+                    .origin(origin)
+                    .destination(destination)
+                    .overview(DirectionsCriteria.OVERVIEW_FULL)
+                    .profile(DirectionsCriteria.PROFILE_DRIVING)
+                    .accessToken(getString(R.string.mapbox_access_token))
+                    .build();
 
-
-    
-
-    public void initializeLocationEngine(){
-
-    }
-
-    public void initialLocationLayer(){
-
-
-    /**
-     * Make a request to the Mapbox Directions API. Once successful, pass the route to the
-     * route layer.
-     * @param mapboxMap the Mapbox map object that the route will be drawn on
-     * @param origin      the starting point of the route
-     * @param destination the desired finish point of the route
-     */
-    private void getRoute(MapboxMap mapboxMap, Point origin, Point destination) {
-        client = MapboxDirections.builder()
-                .origin(origin)
-                .destination(destination)
-                .overview(DirectionsCriteria.OVERVIEW_FULL)
-                .profile(DirectionsCriteria.PROFILE_DRIVING)
-                .accessToken(getString(R.string.mapbox_access_token))
-                .build();
-
-        client.enqueueCall(new Callback<DirectionsResponse>() {
-            @Override
-            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+            client.enqueueCall(new Callback<DirectionsResponse>() {
+                @Override
+                public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
 // You can get the generic HTTP info about the response
-                Timber.d("Response code: " + response.code());
-                if (response.body() == null) {
-                    Timber.e("No routes found, make sure you set the right user and access token.");
-                    return;
-                } else if (response.body().routes().size() < 1) {
-                    Timber.e("No routes found");
-                    return;
-                }
+                    Timber.d("Response code: " + response.code());
+                    if (response.body() == null) {
+                        Timber.e("No routes found, make sure you set the right user and access token.");
+                        return;
+                    } else if (response.body().routes().size() < 1) {
+                        Timber.e("No routes found");
+                        return;
+                    }
 
 // Get the directions route
-                currentRoute = response.body().routes().get(0);
+                    currentRoute = response.body().routes().get(0);
 
 // Make a toast which displays the route's distance
-                Toast.makeText(MapboxMapActivity.this,
-                         (currentRoute.distance())+" meters", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MapboxMapActivity.this,
+                            (currentRoute.distance()) + " meters", Toast.LENGTH_LONG).show();
 
-                if (mapboxMap != null) {
-                    mapboxMap.getStyle(new Style.OnStyleLoaded() {
-                        @Override
-                        public void onStyleLoaded(@NonNull Style style) {
+                    if (mapboxMap != null) {
+                        mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                            @Override
+                            public void onStyleLoaded(@NonNull Style style) {
 
 // Retrieve and update the source designated for showing the directions route
-                            GeoJsonSource source = style.getSourceAs(ROUTE_SOURCE_ID);
+                                GeoJsonSource source = style.getSourceAs(ROUTE_SOURCE_ID);
 
 // Create a LineString with the directions route's geometry and
 // reset the GeoJSON source for the route LineLayer source
-                            if (source != null) {
-                                source.setGeoJson(LineString.fromPolyline(currentRoute.geometry(), PRECISION_6));
+                                if (source != null) {
+                                    source.setGeoJson(LineString.fromPolyline(currentRoute.geometry(), PRECISION_6));
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
-                Timber.e("Error: " + throwable.getMessage());
-                Toast.makeText(MapboxMapActivity.this, "Error: " + throwable.getMessage(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+                @Override
+                public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                    Timber.e("Error: " + throwable.getMessage());
+                    Toast.makeText(MapboxMapActivity.this, "Error: " + throwable.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        viewMap.onStart();
-    }
+        @Override
+        protected void onStart () {
+            super.onStart();
+            viewMap.onStart();
+        }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        viewMap.onStop();
-    }
+        @Override
+        protected void onStop () {
+            super.onStop();
+            viewMap.onStop();
+        }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        viewMap.onResume();
-    }
+        @Override
+        protected void onResume () {
+            super.onResume();
+            viewMap.onResume();
+        }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        viewMap.onSaveInstanceState(outState);
-    }
+        @Override
+        protected void onSaveInstanceState (@NonNull Bundle outState){
+            super.onSaveInstanceState(outState);
+            viewMap.onSaveInstanceState(outState);
+        }
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        viewMap.onLowMemory();
-    }
+        @Override
+        public void onLowMemory () {
+            super.onLowMemory();
+            viewMap.onLowMemory();
+        }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
 // Cancel the Directions API request
-        if (client != null) {
-            client.cancelCall();
+            if (client != null) {
+                client.cancelCall();
+            }
+            viewMap.onDestroy();
         }
-        viewMap.onDestroy();
-    }
 
-    @Override
-    public void onExplanationNeeded(List<String> list) {
+        @Override
+        public void onExplanationNeeded (List < String > list) {
 
-    }
-
-    @Override
-    public void onPermissionResult(boolean b) {
-        if(b){
-            enableLocation();
         }
-    }
+
+        @Override
+        public void onPermissionResult ( boolean b){
+            if (b) {
+                enableLocation();
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult ( int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        @Override
+        public void getLastLocation
+        (@NonNull LocationEngineCallback < LocationEngineResult > locationEngineCallback) throws
+        SecurityException {
+
+        }
+
+        @Override
+        public void requestLocationUpdates (@NonNull LocationEngineRequest
+        locationEngineRequest, @NonNull LocationEngineCallback < LocationEngineResult > locationEngineCallback, @Nullable Looper
+        looper) throws SecurityException {
+
+        }
+
+        @Override
+        public void requestLocationUpdates (@NonNull LocationEngineRequest
+        locationEngineRequest, PendingIntent pendingIntent) throws SecurityException {
+
+        }
+
+        @Override
+        public void removeLocationUpdates
+        (@NonNull LocationEngineCallback < LocationEngineResult > locationEngineCallback) {
+
+        }
+
+        @Override
+        public void removeLocationUpdates (PendingIntent pendingIntent){
+
+        }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public void getLastLocation(@NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback) throws SecurityException {
-
-    }
-
-    @Override
-    public void requestLocationUpdates(@NonNull LocationEngineRequest locationEngineRequest, @NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback, @Nullable Looper looper) throws SecurityException {
-
-    }
-
-    @Override
-    public void requestLocationUpdates(@NonNull LocationEngineRequest locationEngineRequest, PendingIntent pendingIntent) throws SecurityException {
-
-    }
-
-    @Override
-    public void removeLocationUpdates(@NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback) {
-
-    }
-
-    @Override
-    public void removeLocationUpdates(PendingIntent pendingIntent) {
-
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        
     }
 
 
@@ -386,6 +392,5 @@ SearchBottomSheetView searchBottomSheetView = findViewById(R.id.search_view);
         mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                 new LatLng(location.getLatitude(), location.getLongitude()), 16));
     }*/
-
 
 }
