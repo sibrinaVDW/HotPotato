8c8b6c4f0d272ae37c25817bab2be442fe3af734
JadeyOlivier <55444202+JadeyOlivier@users.noreply.github.com>
2021-11-11 10:18:29 +0200

    Fixed it

--- app/src/main/java/com/example/hotpotato/MapboxMapActivity.java
+++ app/src/main/java/com/example/hotpotato/MapboxMapActivity.java
@@ -1,18 +1,36 @@
 package com.example.hotpotato;
 
+import android.app.PendingIntent;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.style.light.Position;
 import com.mapbox.maps.MapView;
 
+import java.util.List;
 
-public class MapboxMapActivity extends AppCompatActivity {
+
+public class MapboxMapActivity extends AppCompatActivity implements OnMapReadyCallback,LocationEngine,PermissionsListener {
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+    private PermissionsManager permissionsManager;
 
     MapView mapView;
     com.mapbox.mapboxsdk.maps.MapView viewMap;
@@ -35,7 +53,7 @@ public class MapboxMapActivity extends AppCompatActivity {
 
     public void onMapReady(MapboxMap mapboxMap) {
         mapMB = mapboxMap;
-
+        //Position origin = Position.fromCoordinates(mapboxMap.getMyLocation().getLongitude(), mapboxMap.getMyLocation().getLatitude());
     }
 
     @Override
@@ -73,5 +91,75 @@ public class MapboxMapActivity extends AppCompatActivity {
         super.onDestroy();
         viewMap.onDestroy();
     }
+
+    @Override
+    public void onExplanationNeeded(List<String> list) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean b) {
+
+    }
+
+    @Override
+    public void getLastLocation(@NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback) throws SecurityException {
+
+    }
+
+    @Override
+    public void requestLocationUpdates(@NonNull LocationEngineRequest locationEngineRequest, @NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback, @Nullable Looper looper) throws SecurityException {
+
+    }
+
+    @Override
+    public void requestLocationUpdates(@NonNull LocationEngineRequest locationEngineRequest, PendingIntent pendingIntent) throws SecurityException {
+
+    }
+
+    @Override
+    public void removeLocationUpdates(@NonNull LocationEngineCallback<LocationEngineResult> locationEngineCallback) {
+
+    }
+
+    @Override
+    public void removeLocationUpdates(PendingIntent pendingIntent) {
+
+    }
+
+
+    //@SuppressWarnings( {"MissingPermission"})
+   /* private void enableLocationPlugin() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Create an instance of LOST location engine
+            initializeLocationEngine();
+
+            LocationLayerPlugin locationLayerPlugin = new LocationLayerPlugin(viewMap, mapMB, locEngine);
+            locationLayerPlugin.setLocationLayerEnabled(LocationLayerMode.TRACKING);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void initializeLocationEngine() {
+        locEngine = new LostLocationEngine(MapsActivity.this);
+        locEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) {
+            setCameraPosition(lastLocation);
+        } else {
+            locationEngine.addLocationEngineListener(this);
+        }
+    }
+
+    private void setCameraPosition(Location location) {
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.getLatitude(), location.getLongitude()), 16));
+    }*/
 
 }
