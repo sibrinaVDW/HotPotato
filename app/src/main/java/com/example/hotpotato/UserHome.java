7c3ad75b15ad4592820bfe50b4ce61333fcfc020
JadeyOlivier <55444202+JadeyOlivier@users.noreply.github.com>
2021-11-11 17:40:31 +0200

    Tried searching

--- app/src/main/java/com/example/hotpotato/UserHome.java
+++ app/src/main/java/com/example/hotpotato/UserHome.java
@@ -10,5 +10,11 @@ public class UserHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
+                .accessToken(MAPBOX_ACCESS_TOKEN)
+                .query(Point.fromLngLat(-77.03655, 38.89770))
+                .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
+                .build();
     }
 }
